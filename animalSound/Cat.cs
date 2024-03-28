17270ac1082f042ba09700d8a4693e2f172229fe
RoxanaNicoleta <arsene.roxana20@yahoo.com>
2024-03-28 12:15:53 +0200

    Initial commit

---
+++ animalSound/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnimalSounds
+{
+    class Cat : Animal
+    {
+        public Cat(string type) : base(type) { }
+
+        public override string MakeSound()
+        {
+            return "Meow";
+        }
+    }
+}
---
+++ animalSound/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalSounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>
+            {
+                new Cat("Cat"),
+                new Dog("Dog"),
+            };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.Type} makes sound: {animal.MakeSound()}");
+            }
+        }
+    }
+}
